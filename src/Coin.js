83cb70e571a48b4c6683888a0d737ee7fe589f72
Saiful <azadahmedofficial@gmail.com>
2025-09-04 09:42:39 +0600

    Updated UI and mb respo

--- src/Coin.js
+++ src/Coin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 import "./Coin.css";
 const Coin = ({
   name,
@@ -10,28 +11,35 @@ const Coin = ({
   marketcap,
 }) => {
   return (
-    <div>
-      <div className="coin-container">
-        <div className="coin-row">
-          <div className="coin">
-            <img src={image} alt="" />
-
-            <h1> {name}</h1>
+    <div className="coin-container">
+      <div className="coin-row">
+        {/* Top section */}
+        <div className="coin">
+          <img src={image} alt={name} />
+          <div>
+            <h1>{name}</h1>
             <p className="coin-symbol">{symbol}</p>
           </div>
-          <div className="coin-data">
-            <p className="coin-price">${price.toLocaleString()}</p>
-            <p className="coin-volume">{volume.toLocaleString()}</p>
+        </div>
+
+        {/* Data section */}
+        <div className="coin-data">
+          <p className="coin-price">${price.toLocaleString()}</p>
+          <p>
             {priceChange < 0 ? (
-              <p className="coin-percent red"> {priceChange.toFixed(2)} %</p>
+              <span className="coin-percent red">
+                <FaArrowDown className="arrow-icon" /> {priceChange.toFixed(2)}%
+              </span>
             ) : (
-              <p className="coin-percent green ">{priceChange.toFixed(2)} %</p>
+              <span className="coin-percent green">
+                <FaArrowUp className="arrow-icon" /> {priceChange.toFixed(2)}%
+              </span>
             )}
-            <p className="coin-maretcap">
-              {" "}
-              Mkt Cap ${marketcap.toLocaleString()}
-            </p>
-          </div>
+          </p>
+          <p className="coin-volume">Vol: {volume.toLocaleString()}</p>
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
